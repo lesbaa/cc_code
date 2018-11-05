c00fc3183f7bc9d4acc4f55ea71d6af43999d346
Les Moffat <les@lesmoffat.co.uk>
2018-11-05 18:58:53 +0000

    init-commit

--- app/app.js
+++ app/app.js
@@ -1,4 +1,20 @@
-import thing from './thing'
+/* global p5 */
+import Victor from 'victor'
+import { resolve } from 'uri-js';
 
-console.log('working')
-thing()
+const c = document.querySelector('#c')
+
+const ctx = c.getContext('2d')
+const loadImg = (url) => new Promise((res, rej) => {
+  const img = document.createElement('img')
+  img.src = url
+  img.addEventListener('load', () => {
+    resolve(img)
+  })
+})
+
+async function go() {
+  const img = await loadImg()
+}
+
+go()
